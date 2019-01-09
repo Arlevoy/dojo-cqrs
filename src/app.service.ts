0903808bccfa462200ed8075f3c3c440d1c04041
Arlevoy <arthurl@bam.tech>
2019-01-09 20:38:36 +0100

    Create CommandBus

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,17 +3,23 @@ import { ArticleDTO } from './article.dto';
 import { Article } from './article.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateArticleCommand } from './commands/implementations/article-created.command';
 
 @Injectable()
 export class AppService {
   constructor(
     @InjectRepository(Article)
     private readonly articleRepository: Repository<Article>,
+    private readonly commandBus: CommandBus,
   ) {}
   getHello(): string {
     return 'Hello World!';
   }
   storeArticle(articleDTO: ArticleDTO): Promise<Article> {
+    const createArticleCommand = new CreateArticleCommand(articleDTO);
+    this.commandBus.execute(createArticleCommand);
+
     const article = this.articleRepository.create(articleDTO);
     return this.articleRepository.save(article);
   }
